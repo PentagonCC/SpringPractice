9b9a71cf7ad9783355c6d0a12ae48f79b4591ec3
Paramount Pictures <iv-nikita99@mail.ru>
2025-10-21 13:28:36 +0300

    init project

---
+++ src/test/java/org/example/SpringUserService/SpringUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.SpringUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
