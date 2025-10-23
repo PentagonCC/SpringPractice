9677a3d709294f81b25a24965a7a717f30d46cfe
Paramount Pictures <iv-nikita99@mail.ru>
2025-10-23 17:24:05 +0300

    add repository package

--- src/main/java/org/example/SpringUserService/repository/UserRepository.java
+++ src/main/java/org/example/SpringUserService/repository/UserRepository.java
@@ -4,6 +4,14 @@ import org.example.SpringUserService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    void delete(User user);
+
+    User save(User user);
+
+    Optional<User> findById(Long id);
 }
