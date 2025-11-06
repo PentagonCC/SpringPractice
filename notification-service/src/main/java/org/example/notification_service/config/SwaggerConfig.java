6fa9f15eaa4b1ce3f717b20e6e8d958d7dd874df
Paramount Pictures <iv-nikita99@mail.ru>
2025-11-06 14:22:03 +0300

    add documentation with swagger

---
+++ notification-service/src/main/java/org/example/notification_service/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package org.example.notification_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Notification Service API",
+        description = "API управления отправкой уведомлений пользователю",
+        version = "1.0.0"))
+public class SwaggerConfig {
+}
--- notification-service/src/main/java/org/example/notification_service/dto/Message.java
+++ notification-service/src/main/java/org/example/notification_service/dto/Message.java
@@ -1,14 +1,18 @@
 package org.example.notification_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "Сообщение с данными для отправки уведомления")
 public class Message {
     @Email
     @NotBlank
+    @Schema(description = "Почта для отправки уведомления")
     private String userEmail;
     @NotNull
+    @Schema(description = "Статус, используемый для выбора уведомления")
     private MessageStatus status;
 
     public Message() {
