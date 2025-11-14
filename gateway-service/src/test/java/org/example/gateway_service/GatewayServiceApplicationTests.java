0347baade5586bb603161dc2470b2bd67bbc1458
Paramount Pictures <iv-nikita99@mail.ru>
2025-11-14 15:40:27 +0300

    discovery service

--- gateway-service/src/test/java/org/example/gateway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/org/example/gateway_service/GatewayServiceApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+public class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
