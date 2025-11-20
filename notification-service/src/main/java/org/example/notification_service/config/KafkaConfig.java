ae273e4e8704d9005379dcd9953e8d0411fab63b
Paramount Pictures <iv-nikita99@mail.ru>
2025-11-20 10:12:10 +0300

    start with docker compose

--- notification-service/src/main/java/org/example/notification_service/config/KafkaConfig.java
+++ notification-service/src/main/java/org/example/notification_service/config/KafkaConfig.java
@@ -21,7 +21,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "notificationService");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
